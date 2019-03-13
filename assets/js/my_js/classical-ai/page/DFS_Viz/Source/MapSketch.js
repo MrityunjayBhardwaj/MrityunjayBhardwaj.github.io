a3b162011cb0e3416e1af203d712cab16c5d878b
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-03-13 14:17:59 +0530

    fix localStorage Issue

--- assets/js/my_js/classical-ai/page/DFS_Viz/Source/MapSketch.js
+++ assets/js/my_js/classical-ai/page/DFS_Viz/Source/MapSketch.js
@@ -4,6 +4,17 @@ var sketch1 = function(p){
 
     p.preload = function(){
         tile_map = p.loadImage("Tile_map_collision.png");
+        slider = document.getElementById("myRange");
+        output = document.getElementById("value");
+
+        let latest_speed = JSON.parse(localStorage.getItem("speed"));
+
+        if(latest_speed != null){
+            slider.value = latest_speed.speed;
+            // window.alert("this value is here: "+slider.value+" "+latest_speed.speed+" "+4);
+        }
+        timeDueration   = 110*(1/slider.value);
+        MasterFrameRate = 180*(1/slider.value);
     }
     p.setup = function(){
         var canvas  = p.createCanvas(400,400);
@@ -20,8 +31,33 @@ var sketch1 = function(p){
         sstatus = document.getElementById("search_status");
 
 
-        lstorage = localStorage.getItem("testJSON");
+        lstorage   = localStorage.getItem("testJSON");
         customMap  = JSON.parse(lstorage);
+
+        if (customMap === null){
+
+            let values = [];
+
+            for(let i=0;i<GRID_DIM[0];i++){
+                for(let j=0;j<GRID_DIM[1];j++){
+                    let pix = 1;
+                    if (tile_map.get(i,j)[0] < 255)
+                        pix = 0;
+
+                    values.push(pix);
+                }
+            }
+
+            values[start_index] = 2;
+            values[end_index]   = 3;
+
+            let myObj = {"status": values}        
+            myJSON = JSON.stringify(myObj);
+            localStorage.setItem("testJSON", myJSON);
+
+            console.log("its running");
+            window.location.reload();
+        }
 
         console.log(customMap);
 
@@ -57,6 +93,7 @@ var sketch1 = function(p){
 
         NodeArray[start_index].Status = "isStart";
         stack.push(NodeArray[start_index])// add the start node
+
     };
 
     p.draw = function(){
@@ -81,12 +118,18 @@ var sketch1 = function(p){
         // console.log("slider: ",slider/100);
 
 
-        slider = document.getElementById("myRange");
-        output = document.getElementById("value");
+
+
         output.innerHTML = slider.value;
 
         slider.oninput = function (){
             output.innerHTML = slider.value;
+
+            let speed_obj = {"speed": slider.value}        
+            myJSON = JSON.stringify(speed_obj);
+            localStorage.setItem("speed", myJSON);
+
+
         }
         timeDueration   = 110*(1/slider.value);
         MasterFrameRate = 180*(1/slider.value);
